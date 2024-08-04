a1dac1231f5f3f6dfd6b4dd3b1feb70c075cba8a
ethanrutt <ethan.rutt2032@gmail.com>
2024-08-04 15:51:29 -0500

    fixed check-health warnings

--- nvim/lua/ethanrutt/plugins/utility.lua
+++ nvim/lua/ethanrutt/plugins/utility.lua
@@ -9,6 +9,16 @@ return {
         -- 'gc' to comment visual regions/lines
         'numToStr/Comment.nvim',
         opts = {},
+        config = function()
+            require('Comment').setup()
+            vim.keymap.del("n", "gc")
+            vim.keymap.del("n", "gb")
+            local wk = require("which-key")
+            wk.add({
+                { "gb", group = "Comment toggle blockwise" },
+                { "gc", group = "Comment toggle linewise" },
+            })
+        end
     },
 
     {
