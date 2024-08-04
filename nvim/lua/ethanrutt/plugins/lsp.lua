c5eb8422ae44177c80fd0c42a5e06cb404470b80
ethanrutt <ethan.rutt2032@gmail.com>
2024-08-04 16:04:19 -0500

    fixing errors

--- nvim/lua/ethanrutt/plugins/lsp.lua
+++ nvim/lua/ethanrutt/plugins/lsp.lua
@@ -82,7 +82,7 @@ return {
         }
 
         -- Setup neovim lua configuration
-        require('neodev').setup()
+        require('lazydev').setup()
 
         -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
         local capabilities = vim.lsp.protocol.make_client_capabilities()
