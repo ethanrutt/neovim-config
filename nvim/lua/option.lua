1dc493f3a010a89c6fd630aee5b562b9ad3cf501
ethanrutt <ethan.rutt2032@gmail.com>
2025-08-22 15:19:15 -0500

    add better options

--- nvim/lua/option.lua
+++ nvim/lua/option.lua
@@ -1,23 +1,74 @@
 local o = vim.o
 
-vim.g.netrw_banner = 0
 
+--
+-- basic
+--
+vim.g.netrw_banner = 0
 o.number = true
-o.hlsearch = false
-o.breakindent = true
-o.undofile = true
-o.smartcase = true
-o.termguicolors = true
-o.expandtab = true
-o.colorcolumn = "80"
-o.signcolumn = "yes"
-o.shiftwidth = 0
-o.tabstop = 4
-o.expandtab = true
+o.cursorline = true
+o.wrap = false
+o.scrolloff = 10
+o.sidescrolloff = 8
 o.mouse = ""
+o.updatetime = 300
 o.timeoutlen = 300
 
+
+--
+-- indentation
+--
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+o.autoindent = true
+o.breakindent = true
+
+
+--
+-- search
+--
+o.ignorecase = true
+o.smartcase = true
+o.hlsearch = false
+o.incsearch = true
+
+
+--
+-- view
+--
+o.termguicolors = true
+o.colorcolumn = "80"
+o.signcolumn = "yes"
+
+
+--
+-- undodir
+--
+local undodir = vim.fn.expand("~/.nvim/undodir")
+if vim.fn.isdirectory(undodir) == 0 then
+	vim.fn.mkdir(undodir, "p")
+end
+o.undofile = true
+o.undodir = undodir
+
+
+--
+-- file
+--
+o.backup = false
+o.writebackup = false
+o.swapfile = false
+o.autoread = true
+o.autowrite = false
+
+
+--
+-- virtual line
+--
 vim.diagnostic.config({
-    virtual_text = false,
-    virtual_lines = false,
+	virtual_text = false,
+	virtual_lines = false,
 })
