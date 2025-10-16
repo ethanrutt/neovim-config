49320fe00911a229154905b44be0c285d09c024d
ethanrutt <ethan.rutt2032@gmail.com>
2025-10-16 17:16:33 -0500

    update treesitter to main

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,64 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-    branch = "master",
-	event = { "BufRead", "BufNewFile" },
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+	lazy = false,
+	branch = "main",
+	dependencies = {
+		"mason-org/mason.nvim",
+	},
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-		})
-	end,
+	opts_extend = {
+		"ensure_installed",
+	},
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"css",
+			"diff",
+			"dockerfile",
+			"graphql",
+			"git_commit",
+			"git_config",
+			"git_rebase",
+			"gitattributes",
+			"gitignore",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"json5",
+			"jsx",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"ruby",
+			"rust",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+	},
+	config = function(_, opts)
+        require("nvim-treesitter").install(opts.ensure_installed)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(ev)
+                local lang = vim.treesitter.language.get_lang(ev.match)
+
+                if vim.tbl_contains(opts.ensure_installed, lang) then
+                    vim.treesitter.start()
+                end
+            end
+        })
+    end,
 }
