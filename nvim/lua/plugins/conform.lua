459a143c2ac4c1db02dc95e8f57b668f95abc5f1
ethanrutt <ethan.rutt2032@gmail.com>
2025-12-10 06:56:35 -0600

    slim conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,22 +5,18 @@ return {
 	dependencies = {
 		"mason-org/mason.nvim",
 	},
-	keys = {
-		{
-			"<leader>F",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
 	opts = {
-		notify_on_error = true,
-		log_level = vim.log.levels.INFO,
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- add additional formatters here
 		},
 	},
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+
+		vim.keymap.set("n", "<leader>F", function()
+			conform.format({ async = true, lsp_fallback = true }, { desc = "[F]ormat buffer" })
+		end)
+	end,
 }
