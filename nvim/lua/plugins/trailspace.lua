b020cb5257fc97c400e34403afaf3c8102ad8255
ethanrutt <ethan.rutt2032@gmail.com>
2025-12-10 07:19:38 -0600

    split up utils file

---
+++ nvim/lua/plugins/trailspace.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-mini/mini.trailspace",
+	version = "*",
+	config = function()
+		ts = require("mini.trailspace")
+		ts.setup()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function()
+				ts.trim()
+				ts.trim_last_lines()
+			end,
+		})
+	end,
+}
---
+++ nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,8 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-mini/mini.icons", version = "*", opts = {} },
+	opts = {
+		preset = "helix",
+	},
+}
