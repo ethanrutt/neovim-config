516eca0d9fce396a25fa9f669f81119f2751b5b9
ethanrutt <ethan.rutt2032@gmail.com>
2026-01-25 16:33:45 -0600

    redo fzf keybinds

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -38,15 +38,24 @@ return {
 			end
 		end
 
-		map("n", "<leader>ff", smart_find_files, { desc = "[F]ind [F]iles" })
-		map("n", "<leader>fb", fzf.buffers, { desc = "[F]ind [B]uffers" })
-
+		map("n", "<leader>sf", smart_find_files, { desc = "[S]earch [F]iles" })
+		map("n", "<leader>sb", fzf.buffers, { desc = "[S]earch [B]uffers" })
 		map("n", "<leader>sg", fzf.live_grep_native, { desc = "[S]earch with [G]rep" })
+		map("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
+		map("n", "<leader>sc", fzf.commands, { desc = "[S]earch [C]ommands" })
 		map("n", "<leader>/", fzf.lgrep_curbuf, { desc = "[/] Fuzzily search current buffer" })
 
-		map("n", "<leader>gd", fzf.lsp_definitions, { desc = "[G]oto [D]efinition" })
-		map("n", "<leader>gr", fzf.lsp_references, { desc = "[G]oto [R]eferences" })
+		map("n", "<leader>gs", fzf.git_status, { desc = "[G]it [S]tatus" })
 
-		map("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
+		map("n", "<leader>lc", fzf.lsp_code_actions, { desc = "[L]SP [C]ode actions" })
+		map("n", "<leader>ld", fzf.lsp_document_symbols, { desc = "[L]SP [D]ocument symbols" })
+
+		map("n", "<leader>ff", smart_find_files, { desc = "[F]ind [F]iles" })
+		map("n", "<leader>fb", fzf.builtin, { desc = "[F]ind [B]uiltin pickers" })
+		map("n", "<leader>fh", fzf.helptags, { desc = "[F]ind [H]elp" })
+		map("n", "<leader>fm", fzf.manpages, { desc = "[F]ind [M]anpages" })
+		map("n", "<leader>fp", fzf.registers, { desc = "[F]ind in registers to [P]aste" })
+		map("n", "<leader>fq", fzf.quickfix, { desc = "[F]ind in [Q]uickfix" })
+		map("n", "<leader>fl", fzf.loclist, { desc = "[F]ind in [L]oclist" })
 	end,
 }
