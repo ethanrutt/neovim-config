03eea0170d7e40bbb355911bd4b740a0708de313
ethanrutt <ethan.rutt2032@gmail.com>
2026-01-25 16:42:54 -0600

    format

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,15 +1,15 @@
 return {
-  "saghen/blink.cmp",
-  version = "1.*",
-  opts = {
-    keymap = {
-      preset = "super-tab",
-      ["<C-x>"] = { "show", "show_documentation", "hide_documentation" },
-    },
-    completion = {
-      menu = {
-        auto_show = false,
-      },
-    },
-  },
+	"saghen/blink.cmp",
+	version = "1.*",
+	opts = {
+		keymap = {
+			preset = "super-tab",
+			["<C-x>"] = { "show", "show_documentation", "hide_documentation" },
+		},
+		completion = {
+			menu = {
+				auto_show = false,
+			},
+		},
+	},
 }
