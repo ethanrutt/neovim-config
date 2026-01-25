990c8a0e9bbc104215df753ec88511a3daa8bd5d
ethanrutt <ethan.rutt2032@gmail.com>
2026-01-25 16:24:31 -0600

    slim options

--- nvim/lua/option.lua
+++ nvim/lua/option.lua
@@ -1,66 +1,33 @@
 local o = vim.o
 
-vim.cmd.colorscheme("habamax")
-
---
--- basic
---
 vim.g.netrw_banner = 0
+
 o.number = true
 o.cursorline = true
 o.wrap = false
 o.scrolloff = 10
 o.sidescrolloff = 8
-o.mouse = ""
 
---
--- indentation
---
-o.tabstop = 4
-o.softtabstop = 4
-o.shiftwidth = 4
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
 o.expandtab = true
 o.smartindent = true
-o.autoindent = true
-o.breakindent = true
 
---
--- search
---
 o.ignorecase = true
 o.smartcase = true
 o.hlsearch = false
-o.incsearch = true
 
---
--- view
---
 o.termguicolors = true
 o.colorcolumn = "80"
 o.signcolumn = "yes"
 
---
--- undodir
---
-local undodir = vim.fn.expand("~/.nvim/undodir")
-if vim.fn.isdirectory(undodir) == 0 then
-	vim.fn.mkdir(undodir, "p")
-end
-o.undofile = true
-o.undodir = undodir
-
---
--- file
---
-o.backup = false
 o.writebackup = false
 o.swapfile = false
 o.autoread = true
-o.autowrite = false
 
---
--- virtual line
---
+o.mouse = ""
+
 vim.diagnostic.config({
 	virtual_text = false,
 	virtual_lines = false,
