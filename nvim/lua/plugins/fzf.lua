6676efecdfd63f8c136ec9fc92d9d5ea33b1d4be
ethanrutt <ethan.rutt2032@gmail.com>
2026-04-22 07:44:54 -0500

    switch git status and grep keybinds in fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -40,12 +40,12 @@ return {
 
 		map("n", "<leader>f", smart_find_files, { desc = "Search [F]iles" })
 		map("n", "<leader>b", fzf.buffers, { desc = "Search [B]uffers" })
-		map("n", "<leader>s", fzf.live_grep_native, { desc = "Search with [G]rep" })
+		map("n", "<leader>g", fzf.live_grep_native, { desc = "Search with [G]rep" })
 		map("n", "<leader>r", fzf.resume, { desc = "[R]esume Search" })
 		map("n", "<leader>c", fzf.commands, { desc = "Search [C]ommands" })
 		map("n", "<leader>/", fzf.lgrep_curbuf, { desc = "[/] Fuzzily search current buffer" })
 
-		map("n", "<leader>g", fzf.git_status, { desc = "[G]it Status" })
+		map("n", "<leader>s", fzf.git_status, { desc = "Git [S]tatus" })
 
 		map("n", "<leader>l", fzf.lsp_code_actions, { desc = "[L]SP Code actions" })
 	end,
